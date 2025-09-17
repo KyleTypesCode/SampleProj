8c3b919cecfbec637bf4067fe6137a7035217574
KyleTypesCode <iisephkyle@gmail.com>
2025-09-17 02:14:05 +0000

    uh the earlier one

--- SampleProj/Time.py
+++ SampleProj/Time.py
@@ -20,41 +20,43 @@ def compareTime():
     #Variable Initialzation
     log_Status = "Logged Out"
     ShiftStatus = "Pre-Shift"
+    #Update time every cycle
     UpdTime = datetime.datetime.now(tz)
     StrUpdTime = UpdTime.strftime('%c')
     #Range
     TimeBefore = datetime.datetime(2025, 9, 15, 9, 0, 0)
-    TimeAfter = datetime.datetime(2025, 9, 15, 18, 3, 0)
+    TimeAfter = datetime.datetime(2025, 9, 15, 18, 0, 0)
     result =  TimeBefore.time() < UpdTime.time() < TimeAfter.time()  
     
     #Conditions
     if TimeBefore.time() > UpdTime.time():
-        print(ShiftStatus)
-        Status = input("Have you logged in ATGS? Yes or No: ")
-        if Status == "Yes":
-            log_Status = "Logged In"
-            print (log_Status)
-        elif Status == "No":
-            print ("Please log in now")
-            time.sleep(3)
-            webbrowser.open('https://app.atgspay.com/app/user-dashboard')
+        webbrowser.open('https://app.atgspay.com/app/user-dashboard')
+        # print(ShiftStatus)
+        # Status = input("Have you logged in ATGS? Yes or No: ")
+        # if Status == "Yes":
+        #     log_Status = "Logged In"
+        #     print (log_Status)
+        # elif Status == "No":
+            # print ("Please log in now")
+            # time.sleep(3)
+            # webbrowser.open('https://app.atgspay.com/app/user-dashboard')
     elif UpdTime.time() > TimeAfter.time():
         ShiftStatus = "Post-Shift"
-        print(ShiftStatus)
-        Status = input("Have you logged out of ATGS? Yes or No: ")
-        if Status == "Yes":
-            log_Status == "Logged Out"
-            print (log_Status)
-            return True
-        elif Status == "No":
-            print ("Please log out now")
-            time.sleep(3)
-            webbrowser.open('https://app.atgspay.com/app/user-dashboard')
+        webbrowser.open('https://app.atgspay.com/app/user-dashboard')
+        # Status = input("Have you logged out of ATGS? Yes or No: ")
+        # if Status == "Yes":
+        #     log_Status == "Logged Out"
+        #     print (log_Status)
+        #     return True
+        # elif Status == "No":
+        #     print ("Please log out now")
+        #     time.sleep(3)
+            
     else:
         ShiftStatus = "On-Shift"
-        print(ShiftStatus)
+        
 
-    print(f"The time is now: {UpdTime} ")
+    print(f"\r The time is now: {StrUpdTime}, {ShiftStatus}" , end="" )
     return result
 
         
